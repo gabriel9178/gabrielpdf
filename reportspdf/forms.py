02b86a99cbb51a4ad39a77c7362183b5fdee95d8
gabriel9178 <gabrieldiagnostics2023@gmail.com>
2024-05-20 12:27:57 +0530

    Update forms.py

--- reportspdf/forms.py
+++ reportspdf/forms.py
@@ -3,12 +3,12 @@ from .models import LabReport
 
 class LabReportForm(forms.ModelForm):
     name = forms.CharField(max_length=100)  # Add a field for the name
+    normal = forms.BooleanField(label='Normal', required=False)  # Checkbox for 'Normal'
+    pr = forms.BooleanField(label='PR', required=False)  # Checkbox for 'PR'
 
     class Meta:
         model = LabReport
-        fields = ['pdf_file']
-
-
+        fields = ['name', 'pdf_file', 'normal', 'pr']
 
 class LogoutForm(forms.Form):
-    pass        
+    pass       
